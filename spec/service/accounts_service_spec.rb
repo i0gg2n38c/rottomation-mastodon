e9e04d0449df0c24dd12b16c08464d4831ba1cb7
i0gg2n38c <i0gg2n38c@mozmail.com>
2025-05-17 03:11:16 +0000

    Moving helper methods out into spec_helper since they'll probably be shared with other tests

--- spec/service/accounts_service_spec.rb
+++ spec/service/accounts_service_spec.rb
@@ -15,7 +15,7 @@ require_relative '../spec_helper'
 # nd    Get account’s featured tags
 # nd    Get lists containing this account
 # ok    Follow account
-# nd    Unfollow account
+# ok    Unfollow account
 # nd    Remove account from followers
 # nd    Block account
 # nd    Unblock account
@@ -59,37 +59,6 @@ RSpec.describe Mastodon::Service::AccountService do
   let(:user_with_posts_username) { 'nA6Xv' }
   let(:user_with_followers_username) { 'dWRm8' }
   let(:user_following_other_users_username) { 'dWRm8' }
-
-  def generic_new_user_form_data
-    described_class::CreateAccountFormBuilder.new
-                                             .set_agreement(true)
-                                             .with_username(get_random_string(5))
-                                             .with_email("#{get_random_string(6)}@#{get_random_string(6)}.test")
-                                             .with_password(get_random_string(20))
-                                             .with_locale('EN')
-                                             .build
-  end
-
-  def confirm_new_account(username:)
-    logger.log_info(log: 'Fetching account by username to get its ID')
-    found_account = described_class.lookup_account(logger: logger, username: username)
-
-    logger.log_info(log: "Confirming account with user id #{found_account.id} as admin user")
-    Mastodon::Service::AdminAccountsService.confirm_account(logger: logger, admin_auth_context: admin_auth_context,
-                                                            id: found_account.id)
-  end
-
-  def create_confirmed_account(new_user_data:)
-    described_class.register_account(logger: logger, auth_context: admin_auth_context,
-                                     new_user_form_data: new_user_data)
-    logger.log_info(log: 'Fetching account by username to get its ID')
-    created_account = described_class.lookup_account(logger: logger, username: new_user_data[:username])
-
-    logger.log_info(log: "Confirming account with user id #{created_account.id} as admin user")
-    Mastodon::Service::AdminAccountsService.confirm_account(logger: logger, admin_auth_context: admin_auth_context,
-                                                            id: created_account.id)
-    created_account
-  end
 
   # Begin Tests
   it 'can Register an account' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,34 @@ def admin_auth
                                                                    username: 'admin@localhost',
                                                                    password: 'mastodonadmin')
 end
+
+def generic_new_user_form_data
+  described_class::CreateAccountFormBuilder.new
+                                           .set_agreement(true)
+                                           .with_username(get_random_string(5))
+                                           .with_email("#{get_random_string(6)}@#{get_random_string(6)}.test")
+                                           .with_password(get_random_string(20))
+                                           .with_locale('EN')
+                                           .build
+end
+
+def confirm_new_account(username:)
+  logger.log_info(log: 'Fetching account by username to get its ID')
+  found_account = described_class.lookup_account(logger: logger, username: username)
+
+  logger.log_info(log: "Confirming account with user id #{found_account.id} as admin user")
+  Mastodon::Service::AdminAccountsService.confirm_account(logger: logger, admin_auth_context: admin_auth_context,
+                                                          id: found_account.id)
+end
+
+def create_confirmed_account(new_user_data:)
+  described_class.register_account(logger: logger, auth_context: admin_auth_context,
+                                   new_user_form_data: new_user_data)
+  logger.log_info(log: 'Fetching account by username to get its ID')
+  created_account = described_class.lookup_account(logger: logger, username: new_user_data[:username])
+
+  logger.log_info(log: "Confirming account with user id #{created_account.id} as admin user")
+  Mastodon::Service::AdminAccountsService.confirm_account(logger: logger, admin_auth_context: admin_auth_context,
+                                                          id: created_account.id)
+  created_account
+end
