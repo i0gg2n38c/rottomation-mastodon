f99b07d60afed0eecced1428a2d03470579aae91
i0gg2n38c <i0gg2n38c@mozmail.com>
2025-05-19 01:34:35 +0000

    Implementing the driver manager with a Selenium test

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
+require 'concurrent'
 require_relative 'spec_helper'
 
-# Tests
 RSpec.describe Mastodon::Pages::AboutPage do
-  let(:ldw) { Rottomation::RottomationDriverWrapper.new test_name: described_class.to_s }
+  it 'can use the driver manager to open the About page 10 times with < 10 total grid nodes' do
+    test_procedure = proc do |ldw:|
+      page = described_class.new driver: ldw
+      page.get
+      expect(page.driver.driver_instance.title).to include 'About - Mastodon'
+    end
 
-  after { ldw.driver_instance&.quit }
-
-  it 'can be created' do
-    page = described_class.new driver: ldw
-    page.get
-    expect(page.driver.driver_instance.title).to include 'About - Mastodon'
+    pool = Concurrent::FixedThreadPool.new(10)
+    10.times do
+      pool.post do
+        Rottomation::DriverManager.execute_test(test_procedure: test_procedure,
+                                                test_name: 'Testing the driver manager')
+      end
+    end
+    pool.shutdown
+    pool.wait_for_termination
   end
 end
